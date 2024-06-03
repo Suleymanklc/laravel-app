20bbe235334c7f0db1dfa452ec69682217fd7a4c
Suleyman Kilic <suleyman.kilic@aerdata.com>
2024-06-03 15:55:40 +0200

    fix

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -3,11 +3,14 @@ output "ecs_cluster_id" {
   value       = aws_ecs_cluster.ecs_cluster.id
 }
 
-output "ecs_service_id" {
+output "ecs_service_id_laravel" {
   description = "The ID of the ECS service"
-  value       = aws_ecs_service.ecs_service.id
+  value       = aws_ecs_service.larevel.id
 }
-
+output "ecs_service_id_nginx" {
+  description = "The ID of the ECS service"
+  value       = aws_ecs_service.nginx.id
+}
 output "ecs_task_definition_arn" {
   description = "The ARN of the ECS task definition"
   value       = aws_ecs_task_definition.ecs_task.arn
